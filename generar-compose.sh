b4b71a2b9e84badbc9b54056756410c541b7605a
maximopalopoli <mpalopoli@fi.uba.ar>
2025-03-16 20:29:19 -0300

    Make the script write the clients and network config

--- generar-compose.sh
+++ generar-compose.sh
@@ -20,3 +20,26 @@ services:
     networks:
       - testing_net
 " > ${fileName}
+
+for ((i=1; i<=amountOfClients; i++)); do
+    echo "client$i:
+    container_name: client1
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=1
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+" >> ${fileName}
+done
+
+echo "networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+" >> ${fileName}
