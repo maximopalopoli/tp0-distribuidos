2d4997b0b359e9b493ddfb09845dc02b43b6870c
maximopalopoli <mpalopoli@fi.uba.ar>
2025-03-16 20:27:35 -0300

    Make the script print the pre clients config
    The config written in the custom compose file is copied from the
     docker-compose-dev.yaml file

--- generar-compose.sh
+++ generar-compose.sh
@@ -7,3 +7,16 @@ amountOfClients=$2
 
 echo "Nombre del archivo de salida: $1"
 echo "Cantidad de clientes: $2"
+
+echo "name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+" > ${fileName}
