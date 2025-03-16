31e9b8e72bb465bf07730ecf2c9b74754da8de61
maximopalopoli <mpalopoli@fi.uba.ar>
2025-03-16 20:14:11 -0300

    Add initial idea of script (receive and print params)

---
+++ generar-compose.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Recibo el nombre del archivo de salida ($1) y la cantidad de clientes que debería tener ($2)
+
+fileName=$1
+amountOfClients=$2
+
+echo "Nombre del archivo de salida: $1"
+echo "Cantidad de clientes: $2"
