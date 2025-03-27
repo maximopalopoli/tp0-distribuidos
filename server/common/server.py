a72200c1acd416d2738c59a4032f7cc1c34cdf5c
maximopalopoli <mpalopoli@fi.uba.ar>
2025-03-27 08:50:12 -0300

    Revert "Handle variable number of clients in server"
    This reverts commit 063b67c31a0ee74f6f06b567466b19faf7a61955.

--- server/common/server.py
+++ server/common/server.py
@@ -1,7 +1,6 @@
 import socket
 import logging
 import signal
-import select
 
 from common.utils import Bet, store_bets
 
@@ -36,12 +35,10 @@ class Server:
 
         while self.is_running:
             try:
-                ready_to_read, _, _ = select.select([self._server_socket], [], [], 0.5)
-                if ready_to_read:
-                    client_sock = self.__accept_new_connection()
-                    if client_sock:
-                        self.__handle_client_connection(client_sock)
-
+                client_sock = self.__accept_new_connection()
+                # Dont handle the connection if accept failed
+                if client_sock:
+                    self.__handle_client_connection(client_sock)
             except OSError as e:
                 if not self.is_running:
                     logging.info("action: shutdown | result: fail")
